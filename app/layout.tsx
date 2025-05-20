ffc18877b89e66d70cfa60af7d5d3477111e0602
Nushan Kodikara EEB <nushan.k@euroeximbank.com>
2025-05-20 02:21:11 +0000

    added meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <script>
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '9815464585199304'); 
+          fbq('track', 'PageView');
+        </script>
+        <noscript>
+          <img height="1" width="1" 
+          src="https://www.facebook.com/tr?id=9815464585199304&ev=PageView
+          &noscript=1"/>
+        </noscript>
+      </head>
       <body className={inter.className}>
         <Navbar />
         <main>{children}</main>
